f8f4b5095bf3bc85defc3c04aa2f75b7de7319c8
durururuk <zkdltms0913@gmail.com>
2024-07-25 16:11:03 +0900

    Step 1 완성

---
+++ src/FourBasicOperationsCalculator/step1/Main.java
@@ -0,0 +1,8 @@
+package FourBasicOperationsCalculator.step1;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.calculate("/",5,2));
+    }
+}
